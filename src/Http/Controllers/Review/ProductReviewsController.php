0b497d62624141cb8e90e89b63de4127511b5c24
mortezaa <mortezajafari76@gmail.com>
2025-09-30 18:55:53 +0330

    fix:issues

--- src/Http/Controllers/Review/ProductReviewsController.php
+++ src/Http/Controllers/Review/ProductReviewsController.php
@@ -7,7 +7,6 @@ namespace Mortezaa97\Shop\Http\Controllers\Review;
 use App\Enums\Status;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Mortezaa97\Reviews\Http\Resources\ReviewResource;
 use Mortezaa97\Reviews\Models\Review;
 use Mortezaa97\Shop\Models\Product;
@@ -20,12 +19,12 @@ class ProductReviewsController extends Controller
     public function __invoke(Request $request, Product $product)
     {
         $reviews = ReviewResource::collection(
-                Review::where('model_id', $product->id)
-                    ->where('model_type', Product::class)
-                    ->with('createdBy')
-                    ->where('status', Status::APPROVED)
-                    ->paginate()
-            );
+            Review::where('model_id', $product->id)
+                ->where('model_type', Product::class)
+                ->with('createdBy')
+                ->where('status', Status::APPROVED)
+                ->paginate()
+        );
 
         return response()->json($reviews->response()->getData(true));
     }
--- src/Http/Resources/ProductSimpleResource.php
+++ src/Http/Resources/ProductSimpleResource.php
@@ -22,7 +22,7 @@ class ProductSimpleResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-//            'id' => $this->id,
+            'id' => $this->id,
             'name' => $this->name,
             'code' => $this->code,
             'slug' => $this->slug,
