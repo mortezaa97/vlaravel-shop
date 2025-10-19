094878355a99aa08cfef9c7623a0bb6dd863393a
Ali Kareshki <kareshkia+1@gmail.com>
2025-10-19 13:09:29 +0330

    refactor: enhance product filtering logic and introduce validation request

--- src/Http/Controllers/Product/FilterProductsController.php
+++ src/Http/Controllers/Product/FilterProductsController.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Mortezaa97\Shop\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Validation\ValidationException;
+use Mortezaa97\Shop\Http\Requests\FilterProductsRequest;
 use Mortezaa97\Shop\Http\Resources\ProductSimpleResource;
 use Mortezaa97\Shop\Models\Product;
 
@@ -16,124 +18,96 @@ class FilterProductsController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(FilterProductsRequest $request)
     {
         try {
-            $cacheKey = 'products_filter_' . md5(json_encode([
-                'available' => $request->available,
-                'min_price' => $request->min_price,
-                'max_price' => $request->max_price,
-                'categories' => $request->categories,
-                'brands' => $request->brands,
-                'search' => $request->search,
-                'url' => $request->url,
-                'order' => $request->order,
-                'page' => $request->page ?? 1,
-                'tags' => $request->tags,
-            ]));
+            $validated = $request->validated();
 
-            $products = Cache::remember($cacheKey, 1, function () use ($request) { // @todo
-                $variantConditions = function ($q) use ($request) {
-                    $q->where('price', '>', 0)
-                        ->when($request->available, fn ($q) => $q->where('quantity', '>', 0))
-                        ->when($request->min_price, fn ($q) => $q->where(function ($q) use ($request) {
-                            $q->where('price', '>=', $request->min_price)
-                                ->orWhere('sale_price', '>=', $request->min_price);
-                        }))
-                        ->when($request->max_price, fn ($q) => $q->where(function ($q) use ($request) {
-                            $q->where('price', '<=', $request->max_price)
-                                ->orWhere('sale_price', '<=', $request->max_price);
-                        }));
-                };
-
-                $query = Product::query()
-                    ->with(['children' => $variantConditions, 'categories', 'reviews', 'tags'])
-                    ->whereHas('children', function ($q) {
-                        $q->where('quantity', '>', 0)
-                            ->where('price', '>', 0);
-                    })
-                    ->when($request->categories, fn ($q) => $q->whereIn('category_id', json_decode($request->categories, true)))
-                    ->when($request->brands, fn ($q) => $q->whereIn('brand_id', json_decode($request->brands, true)))
-                    ->when($request->search, fn ($q) => $q->where('name', 'like', '%' . $request->search . '%'))
-                    ->when($request->url, function ($q) use ($request) {
-                        $q->whereHas('categories', fn ($query) => $query->where('url', $request->url));
-                    })
-                    ->when($request->has('tags') && ! empty($request->tags), function ($q) use ($request) {
-                        $tags = json_decode($request->tags);
-                        $q->whereHas('tags', function ($q) use ($tags) {
-                            $q->whereIn('slug', $tags);
-                        });
-                    })
-                    ->when($request->has('attributes') ?? null, function ($q) use ($request) {
-                        foreach ($request->attributes as $attributeJson) {
-                            $attribute = json_decode($attributeJson, true);
-                            if ($attribute && isset($request->attributes, $attribute['attribute_value_id'])) {
-                                $q->whereHas('variants', function ($query) use ($attribute) {
-                                    $query->active()->whereHas('attributeVariants', function ($subQuery) use ($attribute) {
-                                        $subQuery->where('attribute_id', $attribute['attribute_id'])
-                                            ->where('attribute_value_id', $attribute['attribute_value_id']);
-                                    });
-                                });
-                            }
-                        }
+            /**
+             * Product Conditions.
+             */
+            $query = Product::query()
+                ->active()
+                ->whereNull('parent_id')
+                ->when($validated['categories'] ?? null, function ($q) use ($validated) {
+                    $q->whereHas('categories', fn ($query) => $query->whereIn('categories.id', $validated['categories']));
+                })
+                ->when($validated['product_name'] ?? null, function ($q) use ($validated) {
+                    $q->where('name', 'like', '%' . $validated['product_name'] . '%');
+                })
+                ->when($validated['category_url'] ?? null, function ($q) use ($validated) {
+                    $category = Category::where('url', $validated['category_url'])->first();
+                    if ($category) {
+                        $categoryIds = $category->getAllDescendantIds();
+                        $q->whereHas('categories', fn ($query) => $query->whereIn('categories.id', $categoryIds));
+                    }
+                })
+                ->when($validated['min_price'] ?? null, function ($q) use ($validated) {
+                    $q->whereHas('children', function ($query) use ($validated) {
+                        $query->where('price', '>=', $validated['min_price']);
                     });
+                })
+                ->when($validated['max_price'] ?? null, function ($q) use ($validated) {
+                    $q->whereHas('children', function ($query) use ($validated) {
+                        $query->where('price', '<=', $validated['max_price']);
+                    });
+                })
+                ->when($validated['attributes'] ?? null, function ($q) use ($validated) {
+                    foreach ($validated['attributes'] as $attributeJson) {
+                        $attribute = json_decode($attributeJson, true);
+                        if ($attribute && isset($attribute['attribute_id'], $attribute['attribute_value_id'])) {
+                            $q->whereHas('children', function ($query) use ($attribute) {
+                                $query->whereHas('attributeProducts', function ($subQuery) use ($attribute) {
+                                    $subQuery->where('attribute_id', $attribute['attribute_id'])
+                                        ->where('attribute_value_id', $attribute['attribute_value_id']);
+                                });
+                            });
+                        }
+                    }
+                })
+                ->with([
+                    'children' => function ($q) {
+                        $q->where('price', '>', 0)->where('quantity', '>', 0);
+                    },
+                    'categories',
+                    'specifications.attribute',
+                    'specifications.value',
+                    'tags',
+                    'attributeProducts',
+                ]);
 
-                // Apply sorting
-                switch ($request->order) {
-                    case 'oldest':
-                        $query->oldest();
-                        break;
-                    case 'highest':
-                        $query->orderByDesc(
-                            Product::select('price')
-                                ->whereNotNull('parent_id')
-                                ->whereColumn('id', 'products.id')
-                                ->where('price', '>', 0)
-                                ->when($request->available, fn ($q) => $q->where('quantity', '>', 0))
-                                ->when($request->min_price, fn ($q) => $q->where(function ($q) use ($request) {
-                                    $q->where('price', '>=', $request->min_price)
-                                        ->orWhere('sale_price', '>=', $request->min_price);
-                                }))
-                                ->when($request->max_price, fn ($q) => $q->where(function ($q) use ($request) {
-                                    $q->where('price', '<=', $request->max_price)
-                                        ->orWhere('sale_price', '<=', $request->max_price);
-                                }))
-                                ->orderBy('price', 'desc')
-                                ->limit(1)
-                        );
-                        break;
-                    case 'newest':
-                        $query->latest();
-                        break;
-                    case 'seen':
-                        $query->orderByDesc('views');
-                        break;
-                    default: // 'lowest' and default
-                        $query->orderBy(
-                            Product::select('price')
-                                ->whereNotNull('parent_id')
-                                ->whereColumn('id', 'products.id')
-                                ->where('price', '>', 0)
-                                ->when($request->available, fn ($q) => $q->where('quantity', '>', 0))
-                                ->when($request->min_price, fn ($q) => $q->where(function ($q) use ($request) {
-                                    $q->where('price', '>=', $request->min_price)
-                                        ->orWhere('sale_price', '>=', $request->min_price);
-                                }))
-                                ->when($request->max_price, fn ($q) => $q->where(function ($q) use ($request) {
-                                    $q->where('price', '<=', $request->max_price)
-                                        ->orWhere('sale_price', '<=', $request->max_price);
-                                }))
-                                ->orderBy('price', 'asc')
-                                ->limit(1)
-                        );
-                }
+            /**
+             * Sorting.
+             */
+            switch ($validated['order'] ?? 'newest') {
+                case 'newest':
+                    $query->latest();
+                    break;
+                case 'most_viewed':
+                    $query->orderByDesc('views');
+                    break;
+                case 'most_liked':
+                    $query->withCount('wishlists as like_count')
+                        ->orderByDesc('like_count');
+                    break;
+                default:
+                    $query->latest();
+            }
 
-                return ProductSimpleResource::collection($query->paginate());
+            /**
+             * Pagination.
+             */
+            $perPage = $validated['per_page'] ?? 15;
+            $products = $query->paginate($perPage)->through(function ($product) {
+                return ProductSimpleResource::make($product);
             });
+
+            return response()->json($products);
+        } catch (ValidationException $e) {
+            // Re-throw validation exceptions so they're handled properly by Laravel
+            throw $e;
         } catch (Exception $e) {
             return response()->json($e->getMessage(), 500);
         }
-
-        return response()->json($products->response()->getData(true));
     }
 }
